0949345f48489181c7a8e12cde16f73b6ba4bbc3
Grace Bianchi <gracebianchi@bren.ucsb.edu>
2023-02-20 19:33:11 -0800

    trying to read in census data

--- solar_fam_shiny/app.R
+++ solar_fam_shiny/app.R
@@ -4,64 +4,90 @@ library(shiny)
 library(ggplot2)
 library(tidyverse)
 library(bslib)
+library(tidycensus)
+library(RJSONIO)
+library(googlesheets4)
+library(tigris) # for shape files
+options(tigris_use_cache = TRUE)
+
+census_api_key("ebab921b3002df9b71881ad6c426f34281ce0e11", overwrite = "TRUE")
+resURL <- "https://api.census.gov/data/2021/acs/acs1?get=NAME,B01001_001E&for=metropolitan%20statistical%20area/micropolitan%20statistical%20area:35620&key=ebab921b3002df9b71881ad6c426f34281ce0e11" 
+ljson <- fromJSON(resURL)
+
+# reads in metro areas
+census_data <- get_acs(geography = "cbsa",  year = 2021, 
+              variables = c("B19013_001E", # median household income
+              "B23025_003E", # total civilian labor force
+              "B23025_004E"),   # employed labor force
+              output = "wide",
+              keep_geo_vars = TRUE) %>% 
+  rename("median_income" = "B19013_001E")
+
+# census metro area shape files
+cbsa_shp <- core_based_statistical_areas() 
+
+real_estate_inputs <- read_sheet("https://docs.google.com/spreadsheets/d/1lmTpSDwVANxdAg5sW87Q8l_gtig7M0EjPPJ6J5_7dwg/edit#gid=519968233")
 
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
   # Application title
-  titlePanel("TITLE HEre "),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel("Put my widgets here",
-                 radioButtons(inputId = "penguin_species",
-                              label = "Select a criteria to map",
-                              choices = c("Real Estate", "Landlord Policy", "Solar IRR", "Climate Risk")), # left value is displayed
-                 ### select input box
-                 "Choice a color",
-                 selectInput(inputId = 'pt_color',
-                             label = "choose your favorite color!",
-                             choices = c("Awesome red" = "red",
-                                         "Pretty purple" = "purple",
-                                         "cornflowerblue",
-                                         "orange"))
-    ), ### end sidebarPanel
-    
-    mainPanel("put my graph here!",
-              plotOutput(outputId = "penguin_plot"),
-              tableOutput(outputId = "penguin_table")
-    ) ### end mainPanel
-    
-  ) ### end sidebarLayout
-  
+  titlePanel("Identifying the best areas for rooftop solar on multifamily housing"),
+  navbarPage("Solar Family", #navbarPage allows us to create our tabs
+             tabPanel("Landing Page", #this is how we add tabs.
+                      sidebarLayout(
+                        sidebarPanel("WIDGETS",
+                                     helpText("Create demographic maps with information from the 2010 US Census."),
+                                     selectInput("var", 
+                                                 label = "Choose a variable to display",
+                                                 choices = c("Population Growth", "Employment Growth", "Rent Change"),
+                                                 selected = "Population Growth"),
+               
+                        ), #End sidebarPanel widgets
+               
+                        mainPanel(h2("Background"),
+                                  h5("Sixth level title"),
+                                  "put my graph here!",
+                                  plotOutput("map"),
+                                  tableOutput(outputId = "census_table"),
+                                  
+                        ), ### end mainPanel
+                        
+                       ) #end sidebar (tab1) layout
+             ), # end tabpanel thing 1
+             tabPanel("Metric Data Exploration",
+             sidebarPanel(" ",
+                          radioButtons(inputId = "criteria_select",
+                                       label = "Select a criteria to map",
+                                       choices = c("Real Estate", "Landlord Policy", "Solar IRR", "Climate Risk", "Health Impacts", "Electricity", "CO2 Emissions")), # left value is displayed
+                          
+             ), # end side bar panel
+             ), # end tabpanel
+             tabPanel("Calculating Scores"),
+             tabPanel("Mapping Results"),
+
+    ) ### end nav bar
 ) ### end fluidPage
 
 
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  penguin_select <- reactive({ ### whatever is in reactive function()
+  criteria_select <- reactive({ ### whatever is in reactive function()
     penguins %>%
-      filter(species == input$penguin_species) # penguin species comes from inputId in radio buttons
+      filter(criteria == input$criteria) # penguin species comes from inputId in radio buttons
   })
   
-  penguin_table <- reactive({
-    penguins %>%
+  census_table <- reactive({
+    census_data %>%
       filter(species == input$penguin_species) %>%
       group_by(sex) %>%
       summarize(mean_flip = mean(flipper_length_mm),
                 mean_mass = mean(body_mass_g))
   }) ### end penguin table
   
-  output$penguin_plot <- renderPlot({
-    ggplot(data = penguin_select(),
-           aes(x = flipper_length_mm, y = body_mass_g )) +
-      geom_point(color = input$pt_color) + ### change color based on selection
-      theme_minimal()
-  })
   
-  output$penguin_table <- renderTable({
-    penguin_table()
+  output$census_table <- renderTable({
+    census_table(input = "map")
   })
 }
 
