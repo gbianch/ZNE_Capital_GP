38590d569c279821b765f91f2151129c5765fb3d
Grace Bianchi <gracebianchi@bren.ucsb.edu>
2023-02-23 21:04:10 -0800

    created input for population size, but output doesnt work

--- solar_fam_shiny/app.R
+++ solar_fam_shiny/app.R
@@ -15,53 +15,78 @@ census_api_key("ebab921b3002df9b71881ad6c426f34281ce0e11", overwrite = "TRUE")
 # census metro area shape files
 cbsa_shp <- core_based_statistical_areas() 
 
-real_estate_inputs <- read_sheet("https://docs.google.com/spreadsheets/d/1lmTpSDwVANxdAg5sW87Q8l_gtig7M0EjPPJ6J5_7dwg/edit#gid=519968233")
+real_estate_metrics2021 <- read_sheet("https://docs.google.com/spreadsheets/d/1lmTpSDwVANxdAg5sW87Q8l_gtig7M0EjPPJ6J5_7dwg/edit#gid=519968233")
 
 
 ui <- fluidPage(
   # Application title
   titlePanel("Identifying the best areas for rooftop solar on multifamily housing"),
   navbarPage("Solar Family", #navbarPage allows us to create our tabs
-             tabPanel("Landing Page", #this is how we add tabs.
+             
+             ### tab 1 - background
+             tabPanel("Landing Page", 
+                      #sidebarLayout(
+                        #sidebarPanel(" ",
+                          mainPanel(
+                            h2("Background"),
+                            h5("Sixth level title"),
+                            plotOutput("map"),
+                            h4("model flow chart") ### Add model image here
+                            ) ### end mainPanel
+                       # ) #end sidebar panel
+                      # ) #end sidebar layour
+             ), # ## END of tab 1
+             
+             
+             ### tab2
+             tabPanel("Coefficient Customization",
                       sidebarLayout(
-                        sidebarPanel("WIDGETS",
-                                     helpText("Create demographic maps with information from the 2010 US Census."),
-                                     selectInput("var", 
-                                                 label = "Choose a variable to display",
-                                                 choices = c("Population Growth", "Employment Growth", "Rent Change"),
-                                                 selected = "Population Growth"),
-               
-                        ), #End sidebarPanel widgets
-               
-                        mainPanel(h2("Background"),
-                                  h5("Sixth level title"),
-                                  "put my graph here!",
-                                  plotOutput("map"),
-                                  tableOutput(outputId = "census_table"),
-                                  
-                        ), ### end mainPanel
-                        
-                       ) #end sidebar (tab1) layout
-             ), # end tabpanel thing 1
-             tabPanel("Metric Data Exploration",
-             sidebarPanel(" ",
-                          radioButtons(inputId = "criteria_select",
-                                       label = "Select a criteria to map",
-                                       choices = c("Real Estate", "Landlord Policy", "Solar IRR", "Climate Risk", "Health Impacts", "Electricity", "CO2 Emissions")), # left value is displayed
+                        sidebarPanel(" ",
+                          # input for population size
+                          textInput(inputId = "PopulationSizeMin",
+                                    label = "Minimum Population Size",
+                                    value = "300,000",
+                                    width = "100px"), # end population input
+                         
+                           # help text under input box
+                          helpText("Format example: 100,000"),
+                          actionButton(inputId = "EnterPopulation", label = "Enter Population Size"),
+                        ), # end sidebar panel
+                        mainPanel(
+                          h4("population min check"),
+                          tableOutput(outputId = "pop_test_table")
                           
-             ), # end side bar panel
+                          ) # end main panel
+                      ) # end sidebar panel
+
              ), # end tabpanel
-             tabPanel("Calculating Scores"),
-             tabPanel("Mapping Results"),
+             tabPanel("Score Visualization"),
+             tabPanel("Raw Data")
 
     ) ### end nav bar
 ) ### end fluidPage
 
 
 
+
+
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  criteria_select <- reactive({ ### whatever is in reactive function()
+  
+  # min population input 
+  population_size <- reactive({
+    req(input$PopulationSizeMin)
+    pop_data_min <- real_estate_metrics2021 %>% 
+      filter(popestimate2021 >= input$PopulationSizeMin)
+    
+  })
+  
+  output$pop_test_table <- renderTable({
+    input$population_size
+
+  })
+  
+  weight_input <- reactive({ ### whatever is in reactive function()
     penguins %>%
       filter(criteria == input$criteria) # penguin species comes from inputId in radio buttons
   })
@@ -75,9 +100,7 @@ server <- function(input, output) {
   }) ### end penguin table
   
   
-  output$census_table <- renderTable({
-    census_table(input = "map")
-  })
+
 }
 
 # Run the application 
