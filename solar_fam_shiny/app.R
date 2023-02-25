b5cfcbc7c70afe4b4d8aad81097b78a9f752d93d
Grace Bianchi <gracebianchi@bren.ucsb.edu>
2023-02-24 22:14:40 -0800

    started to make tmap

--- solar_fam_shiny/app.R
+++ solar_fam_shiny/app.R
@@ -8,19 +8,30 @@ library(tidycensus)
 library(googlesheets4)
 library(tigris) # for shape files
 options(tigris_use_cache = TRUE)
+library(tmap) # interactive map
+# map states and US
+library(sf)
+
+
+
+real_estate_metrics2021 <- read_sheet("https://docs.google.com/spreadsheets/d/1lmTpSDwVANxdAg5sW87Q8l_gtig7M0EjPPJ6J5_7dwg/edit#gid=519968233")
+criteria_unweighted <- read_sheet("https://docs.google.com/spreadsheets/d/1yqjhJvXUcEiC3qiYWkKNlF6NNpYXWg15zZHwmwpS5Q0/edit#gid=1307699202", sheet = "FINAL_total_score") %>% 
+  mutate(cbsa = as.character(cbsa))
 
 #census_api_key("ebab921b3002df9b71881ad6c426f34281ce0e11", overwrite = "TRUE")
 
 # census metro area shape files
-cbsa_shp <- core_based_statistical_areas() 
+msa_shp <- core_based_statistical_areas() %>% 
+  janitor::clean_names() %>% 
+  select(!c(csafp, lsad, cbsafp))
 
-real_estate_metrics2021 <- read_sheet("https://docs.google.com/spreadsheets/d/1lmTpSDwVANxdAg5sW87Q8l_gtig7M0EjPPJ6J5_7dwg/edit#gid=519968233")
-criteria_unweighted <- read_sheet("https://docs.google.com/spreadsheets/d/1yqjhJvXUcEiC3qiYWkKNlF6NNpYXWg15zZHwmwpS5Q0/edit#gid=1307699202", sheet = "FINAL_total_score")
+state_shp <- data(state_laea) %>% 
+  st_transform(state_laes, st_crs(msa_shp))
 
-model_img <- img(src = "model_no_weights.jpg",
-                 alt = "Model Visualization",
-                 width="500%")
-  
+# merge criteria table with shp data
+unwt_criteria_shp <- msa_shp %>% 
+  merge(msa_shp, by.x = "cbsa", by.y = "geoid")
+
 ui <- fluidPage(
   # Application title43
   titlePanel("Identifying the best areas for rooftop solar on multifamily housing"),
@@ -29,20 +40,19 @@ ui <- fluidPage(
              ### tab 1 - background
              tabPanel("Landing Page", 
                       #sidebarLayout(
-                        #sidebarPanel(" ",
-                          mainPanel(
-                            h2("Background"),
-                            h5("Sixth level title"),
-                            plotOutput("map"),
-                           
-                             h4("model flow chart"), 
-                            ### Add model image here
-                            div(img(src = "model_no_weights.jpg", 
-                                    width = "800",
-                                    height = "400"), 
-                                style="text-align: center;")
-                            ) ### end mainPanel
-                       # ) #end sidebar panel
+                      #sidebarPanel(" ",
+                      mainPanel(
+                        h2("Background Info"),
+                        h5("Sixth level title"),
+                        plotOutput("map"),
+                        h4("model flow chart"), 
+                        ### Add model image here
+                        div(img(src = "model_no_weights.jpg", 
+                                width = "800",
+                                height = "400"), 
+                            style="text-align: center;")
+                      ) ### end mainPanel
+                      # ) #end sidebar panel
                       # ) #end sidebar layour
              ), # ## END of tab 1
              
@@ -51,51 +61,63 @@ ui <- fluidPage(
              tabPanel("Coefficient Customization",
                       sidebarLayout(
                         sidebarPanel(" ",
-                          # input for population size
-                          textInput(inputId = "PopulationSizeMin",
-                                    label = "Minimum Population Size",
-                                    value = "300000",
-                                    width = "100px"), # end population input
-                         
-                           # help text under input box
-                          helpText("Format example: 100000"),
-                          actionButton(inputId = "EnterPopulation", label = "Enter Population Size"),
+                                     # input for population size
+                                     textInput(inputId = "PopulationSizeMin",
+                                               label = "Minimum Population Size",
+                                               value = "300000",
+                                               width = "100px"), # end population input
+                                     
+                                     # help text under input box
+                                     helpText("Format example: 100000"),
+                                     actionButton(inputId = "EnterPopulation", label = "Enter Population Size"),
                         ), # end sidebar panel
                         mainPanel(
                           h4("population min check"),
-                          dataTableOutput(outputId = "pop_test_table")
+                          #dataTableOutput(outputId = "pop_test_table"),
+                          tmapOutput(outputId = "unwt_criteria_tmap")
                           
-                          ) # end main panel
+                          
+                        ) # end main panel
                       ) # end sidebar panel
-
+                      
              ), # end tabpanel
              tabPanel("Score Visualization"),
              tabPanel("Raw Data")
-
-    ) ### end nav bar
+             
+  ) ### end nav bar
 ) ### end fluidPage
 
 
 
 
 
-# Define server logic required to draw a histogram
+# Define server logic 
 server <- function(input, output) {
   
   # change min population input to numeric class
   pop_size_input <- reactive({ as.numeric(input$PopulationSizeMin)})
   
-# output data table based on minimum population size input
+  # output data table based on minimum population size input
   output$pop_test_table <- renderDataTable({
     req(input$EnterPopulation) # require action button
     pop_size_list <- real_estate_metrics2021 %>% 
       filter(popestimate2021 > pop_size_input())
-
+    
   })
-  
+  ## updating weights based on inputs
   weight_input <- reactive({ ### whatever is in reactive function()
     penguins %>%
       filter(criteria == input$criteria) # penguin species comes from inputId in radio buttons
+  })
+  
+  
+  output$unwt_criteria_tmap <- renderTmap({
+    tmap_mode("view")
+    tm_shape(state_laea) +
+      tm_polygons(unwt_criteria_shp) 
+    # tm_polygons() +
+    # tm_symbols(shape = tmapIcon, size = 0.15, border.lwd = NA) 
+    
   })
   
   census_table <- reactive({
@@ -107,7 +129,7 @@ server <- function(input, output) {
   }) ### end penguin table
   
   
-
+  
 }
 
 # Run the application 
