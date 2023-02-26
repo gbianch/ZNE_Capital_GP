8a5920de66f24bd18f29e834aa7c55d559c6c73d
Grace Bianchi <gracebianchi@bren.ucsb.edu>
2023-02-25 16:03:44 -0800

    adding inputs for real estate, landlord, electricity, and climate risk weights

--- solar_fam_shiny/app.R
+++ solar_fam_shiny/app.R
@@ -1,5 +1,4 @@
 
-
 library(shiny)
 library(ggplot2)
 library(tidyverse)
@@ -9,9 +8,8 @@ library(googlesheets4)
 library(tigris) # for shape files
 options(tigris_use_cache = TRUE)
 library(tmap) # interactive map
-# map states and US
-library(sf)
-
+tmap_options(check.and.fix = TRUE)
+library(sf) # map states and US
 
 
 real_estate_metrics2021 <- read_sheet("https://docs.google.com/spreadsheets/d/1lmTpSDwVANxdAg5sW87Q8l_gtig7M0EjPPJ6J5_7dwg/edit#gid=519968233")
@@ -20,17 +18,22 @@ criteria_unweighted <- read_sheet("https://docs.google.com/spreadsheets/d/1yqjhJ
 
 #census_api_key("ebab921b3002df9b71881ad6c426f34281ce0e11", overwrite = "TRUE")
 
-# census metro area shape files
+# tidycensus package metro area shape files
 msa_shp <- core_based_statistical_areas() %>% 
   janitor::clean_names() %>% 
   select(!c(csafp, lsad, cbsafp))
 
-state_shp <- states(class = "sf", progress_bar = TRUE) %>% 
-  st_transform(st_crs(msa_shp))
+states_4269 <- st_transform(state_laea, st_crs(msa_shp))
+
+# state shape files from sf packages
+# state_shp <- states(class = "sf", progress_bar = TRUE, shift_geo = TRUE) %>% 
+#   st_transform(st_crs(., msa_shp))
 
 # merge criteria table with shp data
 unwt_criteria_shp <- criteria_unweighted %>% 
-  merge(msa_shp, by.x = "cbsa", by.y = "geoid")
+  merge(msa_shp, by.x = "cbsa", by.y = "geoid") %>% 
+  st_sf()
+
 
 ui <- fluidPage(
   # Application title43
@@ -60,22 +63,53 @@ ui <- fluidPage(
              ### tab2
              tabPanel("Coefficient Customization",
                       sidebarLayout(
-                        sidebarPanel(" ",
+                        sidebarPanel("Model Inputs",
                                      # input for population size
                                      textInput(inputId = "PopulationSizeMin",
                                                label = "Minimum Population Size",
                                                value = "300000",
-                                               width = "100px"), # end population input
+                                               width = "80px"), # end population input
                                      
                                      # help text under input box
                                      helpText("Format example: 100000"),
                                      actionButton(inputId = "EnterPopulation", label = "Enter Population Size"),
+                                     
+                                     ### inputs for weights
+                                     numericInput(inputId = "REWtInput",
+                                               label = "Real Estate Weight",
+                                               value = "0.22",
+                                               width = "80px"), # end RE weight input
+                                     
+                                     numericInput(inputId = "LandlordWtInput",
+                                                label = "Landlord Policy Weight",
+                                                value = "0.00",
+                                                width = "80px"), # end landlord weight input
+                                     
+                                     numericInput(inputId = "ElectrictyWtInput",
+                                                label = "Electricity Weight",
+                                                value = "0.07",
+                                                width = "80px"), # end electricity weight input
+                                     
+                                     numericInput(inputId = "CO2WtInput",
+                                                 label = "CO2 Emissions Weight",
+                                                 value = "0.07",
+                                                 width = "80px"), # end electricity weight input
+                                     
+                                     numericInput(inputId = "ClimateRiskWtInput",
+                                                  label = "Climate Risk Weight",
+                                                  value = "0.07",
+                                                  width = "80px"), # end electricity weight input
+                                     
+                                      
+                                     actionButton(inputId = "EnterWeights", label = "Enter Weights")
+                                     
                         ), # end sidebar panel
                         mainPanel(
-                          h4("population min check"),
+                          h4("RE weight input test"),
                           #dataTableOutput(outputId = "pop_test_table"),
-                          tmapOutput(outputId = "unwt_criteria_tmap")
-                          
+                          textOutput(outputId = "wt_test"),
+                          #tmapOutput(outputId = "unwt_criteria_tmap", width = "100%"),
+                        
                           
                         ) # end main panel
                       ) # end sidebar panel
@@ -104,29 +138,39 @@ server <- function(input, output) {
       filter(popestimate2021 > pop_size_input())
     
   })
-  ## updating weights based on inputs
-  weight_input <- reactive({ ### whatever is in reactive function()
-    penguins %>%
-      filter(criteria == input$criteria) # penguin species comes from inputId in radio buttons
-  })
   
+  ## Weight inputs
+  RE_wt_input <- reactive({(input$REWtInput)})
+  landlord_wt_input <- reactive({(input$LandlordWtInput)})
+  electricity_wt_input <- reactive({(input$ElectrictyWtInput)})
+  co2_wt_input <- reactive({(input$CO2WtInput)})
+  climate_risk_wt_input <- reactive({(input$ClimateRiskWtInput)})
+  
+
+  
+## testing weight inputs
+  output$wt_test<- renderPrint({
+    weight_sum <- sum(RE_wt_input(), landlord_wt_input(), electricity_wt_input(), co2_wt_input(),
+                      climate_risk_wt_input())
+    req(input$EnterWeights)
+    paste0(weight_sum)
+    
+    
+  })
+
   
   output$unwt_criteria_tmap <- renderTmap({
     tmap_mode("view")
-    tm_shape(state_laea) +
-      tm_polygons(unwt_criteria_shp) 
+   # tm_shape(states(class = "sf")) + # distorted state shapes, don't use
+    tm_shape(states_4269) +
+      tm_fill(fill = "grey20") +
+      tm_polygons("unwt_criteria_shp") +
+      tm_fill(col = "real_estate_score", palette = "Blues") +
+      tmap_options(check.and.fix = TRUE)
     # tm_polygons() +
     # tm_symbols(shape = tmapIcon, size = 0.15, border.lwd = NA) 
     
   })
-  
-  census_table <- reactive({
-    census_data %>%
-      filter(species == input$penguin_species) %>%
-      group_by(sex) %>%
-      summarize(mean_flip = mean(flipper_length_mm),
-                mean_mass = mean(body_mass_g))
-  }) ### end penguin table
   
   
   
