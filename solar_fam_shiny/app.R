583d9b6dc9a504473da4a93da87e76af8569800d
Grace Bianchi <gracebianchi@bren.ucsb.edu>
2023-03-07 14:12:19 -0800

    display sum of weights

--- solar_fam_shiny/app.R
+++ solar_fam_shiny/app.R
@@ -2,7 +2,7 @@
 library(shiny)
 library(ggplot2)
 library(tidyverse)
-#library(bslib)
+library(bslib) # shiny theme
 library(tidycensus)
 library(googlesheets4)
 library(tigris) # for shape files
@@ -10,6 +10,8 @@ options(tigris_use_cache = TRUE)
 library(tmap) # interactive map
 tmap_options(check.and.fix = TRUE)
 library(sf) # map states and US
+library(shinydashboard)
+
 
 
 real_estate_metrics2021 <- read_sheet("https://docs.google.com/spreadsheets/d/1lmTpSDwVANxdAg5sW87Q8l_gtig7M0EjPPJ6J5_7dwg/edit#gid=519968233")
@@ -21,7 +23,7 @@ criteria_unweighted <- read_sheet("https://docs.google.com/spreadsheets/d/1yqjhJ
 # tidycensus package metro area shape files
 msa_shp <- core_based_statistical_areas() %>% 
   janitor::clean_names() %>% 
-  select(!c(csafp, lsad, cbsafp))
+  select(!c(csafp, lsad, cbsafp)) #4269
 
 states_4269 <- st_transform(state_laea, st_crs(msa_shp))
 
@@ -31,108 +33,90 @@ states_4269 <- st_transform(state_laea, st_crs(msa_shp))
 
 # merge criteria table with shp data
 unwt_criteria_shp <- criteria_unweighted %>% 
-  merge(msa_shp, by.x = "cbsa", by.y = "geoid") %>% 
-  st_sf()
-
+  merge(msa_shp, by.x = "cbsa", by.y = "geoid")
 
 ui <- fluidPage(
-  # Application title43
-  titlePanel("Identifying the best areas for rooftop solar on multifamily housing"),
-  navbarPage("Solar Family", #navbarPage allows us to create our tabs
-             
+  # Application title
+  titlePanel("title"),
+  navbarPage("solar fam",
+             theme = bs_theme(bootswatch = "cerulean", primary = "#8aab57"),
              ### tab 1 - background
              tabPanel("Landing Page", 
-                      #sidebarLayout(
-                      #sidebarPanel(" ",
-                      mainPanel(
-                        h2("Background Info"),
+                      mainPanel("Background Info",
                         h5("Sixth level title"),
-                        plotOutput("map"),
+                        #plotOutput("map"),
                         h4("model flow chart"), 
                         ### Add model image here
-                        div(img(src = "model_no_weights.jpg", 
-                                width = "800",
-                                height = "400"), 
-                            style="text-align: center;")
+                        div(img(src = "model_no_weights.jpg",  width = "800", height = "400"), style="text-align: center;")
                       ) ### end mainPanel
-                      # ) #end sidebar panel
-                      # ) #end sidebar layour
              ), # ## END of tab 1
-             
-             
+            
              ### tab2
              tabPanel("Coefficient Customization",
-                      sidebarLayout(
-                        sidebarPanel("Model Inputs",
-                                     # input for population size
-                                     textInput(inputId = "PopulationSizeMin",
-                                               label = "Minimum Population Size",
-                                               value = "300000",
-                                               width = "80px"), # end population input
-                                     
-                                     # help text under input box
-                                     helpText("Format example: 100000"),
-                                     actionButton(inputId = "EnterPopulation", label = "Enter Population Size"),
-                                     
-                                     ### inputs for weights
-                                     numericInput(inputId = "REWtInput",
-                                               label = "Real Estate Weight",
-                                               value = "0.22",
-                                               width = "80px"), # end RE weight input
-                                     
-                                     numericInput(inputId = "LandlordWtInput",
-                                                label = "Landlord Policy Weight",
-                                                value = "0.00",
-                                                width = "80px"), # end landlord weight input
-                                     
-                                     numericInput(inputId = "ElectrictyWtInput",
-                                                label = "Electricity Weight",
-                                                value = "0.07",
-                                                width = "80px"), # end electricity weight input
-                                     
-                                     numericInput(inputId = "CO2WtInput",
-                                                 label = "CO2 Emissions Weight",
-                                                 value = "0.07",
-                                                 width = "80px"), # end Co2 weight input
-                                     
-                                     numericInput(inputId = "ClimateRiskWtInput",
-                                                  label = "Climate Risk Weight",
-                                                  value = "0.07",
-                                                  width = "80px"), # end climate risk weight input
-                                     
-                                     numericInput(inputId = "HealthWtInput",
-                                                  label = "Health Impacts Weight",
-                                                  value = "0.07",
-                                                  width = "80px"), # end electricity weight input
-                                     
-                                     numericInput(inputId = "solarIrrWtInput",
-                                                  label = "Solar IRR Weight",
-                                                  value = "0.07",
-                                                  width = "80px"), # end electricity weight input
-                                     
-                                      
-                                     actionButton(inputId = "EnterWeights", label = "Calculate")
-                                     
-                        ), # end sidebar panel
+                    box(title = "Criteria Weights", width = 6, status = "primary",
+                          fluidRow(column(2,
+                                          ### inputs for weights
+                                          numericInput(inputId = "REWtInput", label = "Real Estate",
+                                                       value = 0,width = "100px", min = 0.0,max = 1.0,step = 0.05) # end RE weight input
+                                          ), # end RE fcolumn
+                                   column(2, numericInput(inputId = "LandlordWtInput", label = "Landlord Policy",
+                                                       value = 0,
+                                                       width = "100px",
+                                                       min = 0.0,max = 1.0,step = 0.05) # end landlord weight input
+                                          ), # end landlord fluid row
+                                   column(2, numericInput(inputId = "ElectrictyWtInput",
+                                                          label = "Electricity",
+                                                          value = 0,width = "100px",
+                                                          min = 0.0, max = 1.0, step = 0.05) # end electricity weight input
+                                          )), #  fluid row 1
+                          fluidRow(column(2, numericInput(inputId = "CO2WtInput",
+                                                          label = "CO2 Emissions",value = 0,width = "100px",
+                                                          min = 0.0, max = 1.0,step = 0.05) # end Co2 weight input
+                                          ), # end Co2 fluid row
+                                   column(2, numericInput(inputId = "ClimateRiskWtInput",
+                                                          label = "Climate Risk",value = 0,width = "100px",
+                                                          min = 0.0, max = 1.0, step = 0.05) # end climate risk weight input
+                                          ), # end climate risk input
+                                   column(2, numericInput(inputId = "HealthWtInput",
+                                                             label = "Health Impacts",value = 0,
+                                                             width = "100px",min = 0.0,max = 1.0, step = 0.05) # end health impact weight input
+                                          )), # end health impacts input
+                          fluidRow(column(4, numericInput(inputId = "SolarIrrWtInput",
+                                                          label = "Solar IRR",
+                                                          value = 0,
+                                                          width = "100px",
+                                                          min = 0.0, max = 1.0, step = 0.05) # end irr input
+                                          )), # end IRR fluid row
+                        fluidRow(column(2, actionButton(inputId = "EnterWeights", label = "Calculate")))
+                          ), # end box
+                  
                         mainPanel(
-                          h4("RE weight input test"),
-                          #dataTableOutput(outputId = "pop_test_table"),
-                          textOutput(outputId = "wt_test"),
-                         plotOutput(outputId = "wt_criteria_chart"),
-                          #tmapOutput(outputId = "unwt_criteria_tmap", width = "100%"),
-                        
-                          
-                        ) # end main panel
-                      ) # end sidebar panel
+                          verbatimTextOutput(outputId = "wt_sum"),
+                          dataTableOutput(outputId = "wt_table")
+                          # tabsetPanel(
+                          #   id = "tabset",
+                          #   tabPanel("Barchart",)),
+                          #   tabPanel("Map", "two"),
+                          #   tabPanel("Table", "table"))
+                          # ) #end tabset panel 
+                        ) # tab 1end main panel
                       
-             ), # end tabpanel
-             tabPanel("Score Visualization"),
-             tabPanel("Raw Data")
+             ), # end 2 tabpanel
+             tabPanel("Map Visualization"
+                      ), # end map viz tab Panel 
              
+             tabPanel("Demographic Data",
+                      textInput(inputId = "PopulationSizeMin",
+                                label = "Minimum Population Size",
+                                value = "300000",
+                                width = "100px"), # end population input)
+                      # help text under input box
+                      helpText("Format example: 100000"),
+                      actionButton(inputId = "EnterPopulation", label = "Enter Population Size")
+             ) # end tab panel
+  
   ) ### end nav bar
 ) ### end fluidPage
-
-
 
 
 
@@ -147,65 +131,49 @@ server <- function(input, output) {
     req(input$EnterPopulation) # require action button
     pop_size_list <- real_estate_metrics2021 %>% 
       filter(popestimate2021 > pop_size_input())
-    
   })
-  
-  ## Weight inputs
-  RE_wt_input <- reactive({(input$REWtInput)})
-  landlord_wt_input <- reactive({(input$LandlordWtInput)})
-  electricity_wt_input <- reactive({(input$ElectrictyWtInput)})
-  co2_wt_input <- reactive({(input$CO2WtInput)})
-  ClimateRisk_wt_input <- reactive({(input$ClimateRiskWtInput)})
-  health_wt_input <- reactive({(input$HealthWtInput)})
-  solarIRR_wt_input <- reactive({(input$solarIrrWtInput)})
-  
 
-  
-## testing weight inputs
-  output$wt_test<- renderPrint({
-    req(input$EnterWeights)
-    weight_sum <- sum(RE_wt_input(), landlord_wt_input(), electricity_wt_input(), co2_wt_input(),
-                      ClimateRisk_wt_input(), health_wt_input(), solarIRR_wt_input())
-    # require calculate button to be pressed before returning anything
-    req(input$EnterWeights)
-    # if weights don't equal 1 return error
-    if (weight_sum != 1){
-      paste0("Weights must sum to 1.")
-    }
-    # if weights equal 1, multiply them by corresponding scores
-    else {
-      weighted_criteria <- unwt_criteria_shp %>% 
-        mutate(wt_RE = RE_wt_input()*real_estate_score,
-               wt_landlord = landlord_wt_input()*landlord_score,
-               wt_electricity = electricity_wt_input()*electricity_score,
-               wt_co2 = co2_wt_input()*co2_score,
-               wt_climate_risk = climate_risk_wt_input()*climate_risk_score,
-               wt_health = health_wt_input()*health_impact_score,
-               wt_solarIRR = solarIRR_wt_input()*financial_score,
-               total = sum(cols(starts_with("wt_")))) %>% 
-        # removed unweighted columns for barchart
-        select(!c(ends_with("_score"))) %>% 
-        pivot_longer(cols = starts_with("wt"), # selecting criteria cols
-                     names_to = "criteria",
-                     values_to = "wt_criteria_score") %>% 
-        # filter(msa_state != "Portland, ME") %>%  # removing portland (negative number)
-        group_by(cbsa)
-    } # end else
-    
+  weight_inputs <- reactive({
+    c(input$REWtInput, input$LandlordWtInput,
+                             input$ElectrictyWtInput,
+                             input$CO2WtInput,
+                             input$ClimateRiskWtInput,
+                             input$HealthWtInput,
+                             input$SolarIrrWtInput)  
   })
+  
+  # text to show weight sum
+  output$wt_sum <- renderPrint({
+    sum(weight_inputs())
+  })
+
+
+  ########################################################################
+
+### method 2 with observe and creating table   
+  # Update reactive values when weights are changed
+  
+  # Render the weighted criteria table
+  output$wt_table <- renderDataTable({
+    req(input$EnterWeights)
+    unwt_criteria_shp
+
+  })
+  ####################################################################
 
   ## output weighted criteria into bar chart
   output$wt_criteria_chart <- renderPlot({
-    ggplot(data = weighted_criteria, aes(y = reorder(msa_state, total), x = criteria_score)) + 
-      geom_col(aes(fill = criteria)) +
-      labs(x = "Total Score",
-           y = " ",
-           title = " ") +
+    wt_data <- wt_criteria()
+    req(input$EnterWeights)
+        ggplot(data = wt_criteria(), aes(y = reorder(msa, total), x = wt_criteria_score)) + 
+          geom_col(aes(fill = criteria)) +
+          labs(x = "Total Score",y = " ", title = " ") +
       theme_bw() +
       guides(fill=guide_legend(title="Criteria (weighted)")) +
       scale_fill_manual(values = c("palevioletred1", "orange",  "gold", "#44AA99", "dodgerblue4", "#6495ED", "#BC80BD"),
                         labels=c('Climate Risk', 'CO2 Emissions', 'Electricity', "Solar IRR", "Real Estate", "Health Impacts", "Landlord Policy")) +
       scale_x_continuous(limits = c(0, 1.0), expand = c(0,0), breaks=seq(0, 01.0, 0.1))
+
   })
   
   output$unwt_criteria_tmap <- renderTmap({
@@ -218,15 +186,14 @@ server <- function(input, output) {
       tmap_options(check.and.fix = TRUE)
     # tm_polygons() +
     # tm_symbols(shape = tmapIcon, size = 0.15, border.lwd = NA) 
-    
   })
-  
   
   
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
 
 
 
